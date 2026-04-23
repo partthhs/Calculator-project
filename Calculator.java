a8c6a52864e728dd3a2d6bca5dccd52bf99df364
Parth Sakpal <parthh989@gmail.com>
2026-04-24 00:02:36 +0530

    Refactor Calculator class to Calcy with improvements

--- Calculator.java
+++ Calculator.java
@@ -2,54 +2,75 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calculator extends JFrame implements ActionListener{
-     JTextField textField;
+public class Calcy extends JFrame implements ActionListener {
 
-     double num1=0,num2=0,result=0;
+    JTextField textField;
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
 
-     char operator;
+    Calcy() {
+        setTitle("Simple Calculator");
+        setSize(300, 400);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-     Calculator(){
-setTitle("Simple Calculator");
-setSize(300,400);
-setLayout(null);
+        textField = new JTextField();
+        textField.setBounds(30, 40, 220, 40);
+        textField.setFont(new Font("Arial", Font.BOLD, 20));
+        add(textField);
 
-setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        String buttons[] = {
+            "7","8","9","/",
+            "4","5","6","*",
+            "1","2","3","-",
+            "0","C","=","+"
+        };
 
-textField = new JTextField();
+        int x = 30, y = 100;
 
-textField.setBounds(30,40,220,40);
-textField.setFont(new Font("Arial",Font.BOLD,20));
-add(textField);
+        for (int i = 0; i < 16; i++) {
+            JButton btn = new JButton(buttons[i]);
+            btn.setBounds(x, y, 50, 50);
+            btn.addActionListener(this);
+            add(btn);
 
-String buttons[] = {
-    "7","8","9","/","4","5","6","*","1","2","3","-","0","C","=","+"
-};
+            x += 60;
+            if ((i + 1) % 4 == 0) {
+                x = 30;
+                y += 60;
+            }
+        }
 
-int x = 30,y=100;
+        setVisible(true);
+    }
 
-       for(int i =0;i <16;i++){
-         JButton btn = new JButton(button[i]);
-         btn.setBounds(x,y,50,50);
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
 
-         btn.addActionListener(this);
-         add(btn);
+        if (command.charAt(0) >= '0' && command.charAt(0) <= '9') {
+            textField.setText(textField.getText() + command);
+        } else if (command.equals("C")) {
+            textField.setText("");
+        } else if (command.equals("=")) {
+            num2 = Double.parseDouble(textField.getText());
 
-         x +=60;
-         if((i+1)%4==0){
-           x=30;
-           y+=60;
-         }}
-       setVisible(true);
-     }
-  public void actionPerfomed(ActionEvent e){
-    String command = e.getActionCommand();
+            switch (operator) {
+                case '+': result = num1 + num2; break;
+                case '-': result = num1 - num2; break;
+                case '*': result = num1 * num2; break;
+                case '/': result = num1 / num2; break;
+            }
 
-    if(command.charAt(0) >='0' && command.charAt(0)<='9'){
+            textField.setText("" + result);
+        } else {
+            num1 = Double.parseDouble(textField.getText());
+            operator = command.charAt(0);
+            textField.setText("");
+        }
+    }
 
-      textField.set
-
+    public static void main(String[] args) {
+        new Calcy();
+    }
 }
-
-     }
     
