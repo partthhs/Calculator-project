4eaca11611e4208c41c3f25c78bcb28da6974b47
Parth Sakpal <parthh989@gmail.com>
2026-04-23 23:41:21 +0530

    Implement basic calculator GUI in Currency.java

---
+++ Currency.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator extends JFrame implements ActionListener{
+     JTextField textField;
+
+     double num1=0,num2=0,result=0;
+
+     char operator;
+
+     Calculator(){
+setTitle("Simple Calculator");
+setSize(300,400);
+setLayout(null);
+
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+textField = new JTextField();
+
+textField.setBounds(30,40,220,40);
+textField.setFont(new Font("Arial",Font.BOLD,20));
+add(textField);
+
+String buttons[] = {
+    "7","8","9","/","4","5","6","*","1","2","3","-","0","C","=","+"
+};
+
+int x = 30,y=100;
+
+       for(int i =0;i <16;i++){
+         JButton btn = new JButton(button[i]);
+         btn.setBounds(x,y,50,50);
+
+         btn.addActionListener(this);
+         add(btn);
+
+         x +=60;
+         if((i+1)%4==0){
+           x=30;
+           y+=60;
+         }}
+       setVisible(true);
+     }
+  public void actionPerfomed(ActionEvent e){
+    String command = e.getActionCommand();
+
+    if(command.charAt(0) >='0' && command.charAt(0)<='9'){
+
+      textField.set
+
+}
+
+     }
+    
